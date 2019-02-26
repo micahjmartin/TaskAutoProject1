dc646e13d434c6851f7e1215eeced2ad5d8cb8a9
Paul Hulbert <paulhockey77@gmail.com>
2019-02-26 14:53:05 -0500

    Add grep

--- apm.sh
+++ apm.sh
@@ -2,11 +2,18 @@
 
 secondCount=0
 
+PIDS="watchdog kthreadd"
+
 while [ $secondCount -lt 901 ]
 do
 	echo $secondCount
-	ps | tail -n +2 > temp.txt
-	cat temp.txt
+	ps aux > temp.txt
+	
+	for PID in $PIDS
+	do
+		grep "$PID" temp.txt | awk '{ "$secondCount," print $3 "," $4 }' | cat
+	done
+	
 	secondCount=$(($secondCount + 5))
 	rm temp.txt
 	
