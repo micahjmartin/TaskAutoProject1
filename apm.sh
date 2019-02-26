86b4d43a38f7aaa5f291cf4666dc4da258966555
Jonathan Jang <jwj3767@rit.edu>
2019-02-26 16:21:01 -0500

    Jon

--- apm.sh
+++ apm.sh
@@ -4,22 +4,22 @@
 # Launches all of the given programs and monitors them
 # Authors: Micah Martin (mjm5097), Paul Hulbert (), Jonathan Jang (jwj3737)
 
-# Launch all of the needed programs for APM to monitor
 function launch_programs() {
+    # Launch all of the needed programs for APM to monitor
     programs="APM1 APM2 APM3 APM4 APM5 APM6"
-    # Identify the IP address of the 
+	# Identify the IP address of the 
+    ip=`ifconfig | grep "eth3" -A 1 | tail -n 1 | awk '{print $2}'`
     echo "[*] Spawning processes...."
-    echo -n "[+] Launched PID's"
+	echo -n "[+] Launched PID's"
     for prog in $programs; do
-        ./$prog $1 &>/dev/null & 
+        ./$prog $ip &>/dev/null & 
         pid=$!
         echo -n " $pid"
         PIDS="$PIDS $pid"
     done
-    echo ""
+	echo ""
 }
 
-# Kill all the spawned processes and then exit
 function cleanup() {
     echo " [*] Cleaning up spawned processes and exiting...."
     echo -n "[+] Killed process"
@@ -32,39 +32,65 @@ function cleanup() {
 }
 
 function main() {
-    # Definitions for easy reference
-    sleepTime=5
-    outputProcessFile="process_output.txt"
-    echo "[*] Starting process monitoring for 15 minutes"
-    while [ $SECONDS -le 900 ]
-    do
-        # Get all the PS information
-        #pids=`echo $PIDS | tr ' ' ','`
-        #ps aux > temp.txt
-        result="$SECONDS"
-        # Get the %CPU and %MEM from the PIDs
-        for PID in $PIDS; do
-            result="$result,`ps -p $PID -o pcpu,pmem | tail -n+2 | awk '{print $1 "," $2}'`"
-        done
-        echo $result >> $outputProcessFile
+	secondCount=0
+	sleepTime=5
+	while [ $secondCount -lt 901 ]
+	do
+		# Get all the PS information
+		ps aux > temp.txt
+		result="$secondCount"
+		# Get the %CPU and %MEM from the PIDS
+		for PID in $PIDS; do
+			result="$result,`grep "$PID" temp.txt |  awk '{print $3 "," $4}'`"
+		done
+		
+		echo $result
+		rm temp.txt
+		
+		#END OF PAUL'S PART
+		
+		# System Level Metrics written to file: system_metrics.csv
+		# <seconds>, <RX data rate>, <TX data rate>, <disk writes>, <available disk capacity>
+		
+		# Collects Network Bandwidth Utilization, Hard disk access rates, & Hard disk utilization
+		
+		# RX Data Rate & TX Data Rate
+		#RX_DATA=$(cat /sys/class/net/eno16777736/statistics/rx_bytes)
+		#TX_DATA=$(cat /sys/class/net/eno16777736/statistics/tx_bytes)
+		$(ifstat -d 1)
+		$(ifstat eno16777736 >> jtmp.txt)
+		$(head -n -1 jtmp.txt >> jtmp1.txt)
+		$(tail -n +4 jtmp1.txt >> jtmp2.txt)
+		$(rm jtmp.txt jtmp1.txt)
+		$(mv jtmp2.txt jtmp.txt)
+		
+		RX_DATA=$(awk '$6 {print $6}' jtmp.txt)
+		RX_RATE=$(awk '$7 {print $7}' jtmp.txt)
+		
+		RX=$(( $RX_DATA / $RX_RATE ))
 
-        # TODO: Get the system stats here
+		TX_DATA=$(awk '$8 {print $8}' jtmp.txt)
+		TX_RATE=$(awk '$9 {print $9}' jtmp.txt)
+		
+		TX=$(( $RX_DATA / $RX_RATE ))
 
-        sleep $sleepTime
-    done
+		
+		echo $secondCount " " >> system_metrics.csv
+
+		# Disk Writes
+		
+		# Disk Capacity
+
+
+		#END OF JON'S PART
+		secondCount=$(($secondCount + $sleepTime))
+		echo "[*] Sleeping for $sleepTime seconds..."
+		sleep $sleepTime
+	done
 }
 
-# Make sure our args are passed in
-[ "$1" = "" ] && echo "USAGE $0 <ip address>" && exit
 
-# Register the trap
 trap cleanup INT
-
-# Launch all the programs initially
-launch_programs $1
-
-# Call the main
+launch_programs
 main
-
-# Call the cleanup if the main every completes
 cleanup
