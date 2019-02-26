75149f9f4d4146fad1b1df876e7f263e639c6695
Paul Hulbert <paulhockey77@gmail.com>
2019-02-26 14:30:45 -0500

    Add tail to temp

--- apm.sh
+++ apm.sh
@@ -5,7 +5,9 @@ secondCount=0
 while [ $secondCount -lt 901 ]
 do
 	echo $secondCount
-	ps | echo
+	ps | tail -n +2 > temp.txt
+	cat temp.txt
 	secondCount=$(($secondCount + 5))
+	rm temp.txt
 	sleep 5
 done
