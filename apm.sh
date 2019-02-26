b15d9a05ad6e93024eba847553e5ca832c37c154
Micah J. Martin <mjm5097@rit.edu>
2019-02-26 15:39:55 -0500

    Cleaned up and fixed ip, and seconds

--- apm.sh
+++ apm.sh
@@ -7,17 +7,16 @@
 function launch_programs() {
     # Launch all of the needed programs for APM to monitor
     programs="APM1 APM2 APM3 APM4 APM5 APM6"
-	# Identify the IP address of the 
-    ip=`ifconfig | grep "eth3" -A 1 | tail -n 1 | awk '{print $2}'`
+    # Identify the IP address of the 
     echo "[*] Spawning processes...."
-	echo -n "[+] Launched PID's"
+    echo -n "[+] Launched PID's"
     for prog in $programs; do
-        ./$prog $ip &>/dev/null & 
+        ./$prog $1 &>/dev/null & 
         pid=$!
         echo -n " $pid"
         PIDS="$PIDS $pid"
     done
-	echo ""
+    echo ""
 }
 
 function cleanup() {
@@ -32,38 +31,46 @@ function cleanup() {
 }
 
 function main() {
-	secondCount=0
-	sleepTime=5
-	while [ $secondCount -lt 901 ]
-	do
-		# Get all the PS information
-		ps aux > temp.txt
-		result="$secondCount"
-		# Get the %CPU and %MEM from the PIDS
-		for PID in $PIDS; do
-			result="$result,`grep "$PID" temp.txt |  awk '{print $3 "," $4}'`"
-		done
-		
-		echo $result
-		rm temp.txt
-		
-		#END OF PAUL'S PART
-		
-		
-		
-		
-		
-		
-		
-		#END OF JON'S PART
-		secondCount=$(($secondCount + $sleepTime))
-		echo "[*] Sleeping for $sleepTime seconds..."
-		sleep $sleepTime
-	done
+    sleepTime=5
+    echo "[*] Starting process monitoring at $SECONDS seconds"
+    while [ $SECONDS -lt 901 ]
+    do
+        # Get all the PS information
+        ps aux > temp.txt
+        result="$SECONDS"
+        # Get the %CPU and %MEM from the PIDS
+        for PID in $PIDS; do
+            result="$result,`grep "$PID" temp.txt |  awk '{print $3 "," $4}'`"
+        done
+        
+        echo $result
+        rm temp.txt
+        
+        #END OF PAUL'S PART
+        
+        
+        
+        
+        
+        
+        
+        #END OF JON'S PART
+        echo "[*] Sleeping for $sleepTime seconds..."
+        sleep $sleepTime
+    done
 }
 
+# Make sure our args are passed in
+[ "$1" = "" ] && echo "USAGE $0 <ip address>" && exit
 
+# Register the trap
 trap cleanup INT
-launch_programs
+
+# Launch all the programs initially
+launch_programs $1
+
+# Call the main
 main
+
+# Call the cleanup if the main every completes
 cleanup
